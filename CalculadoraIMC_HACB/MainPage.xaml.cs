7f524e139dffdff740c4a878a8e578cf6b304da5
Harol Cisneros <harolcsnrs@gmail.com>
2024-10-03 12:40:11 -0700

    Agregar archivos de proyecto.

---
+++ CalculadoraIMC_HACB/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraIMC_HACB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var peso = float.Parse(Peso.Text);
+            var altura = float.Parse(Altura.Text);
+
+            var imc=peso/(altura*altura);
+
+            IMC.Text = imc.ToString();
+
+        }
+    }
+}
