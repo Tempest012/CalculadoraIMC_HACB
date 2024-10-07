1b903ad877f292f483a2c460039413ebc25e97b7
Harol Cisneros <harolcsnrs@gmail.com>
2024-10-07 12:58:20 -0700

    Unidad1_Practica3: Rangos y Validaciones

--- CalculadoraIMC_HACB/MainPage.xaml.cs
+++ CalculadoraIMC_HACB/MainPage.xaml.cs
@@ -17,12 +17,38 @@ namespace CalculadoraIMC_HACB
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            var peso = float.Parse(Peso.Text);
-            var altura = float.Parse(Altura.Text);
+            if (!string.IsNullOrEmpty(Altura.Text) && !string.IsNullOrEmpty(Peso.Text)) 
+            {
+                var peso = float.Parse(Peso.Text);
+                var altura = float.Parse(Altura.Text);
 
-            var imc=peso/(altura*altura);
+                var imc = peso / (altura * altura);
 
-            IMC.Text = imc.ToString();
+                IMC.Text = imc.ToString();
+
+                string resultado = "";
+                if (imc < 18.5)
+                {
+                    resultado = "Tiene bajo peso";
+                }
+                else if (imc >= 18.5 && imc <= 29.9)
+                {
+                    resultado = "Tu peso es normal";
+                }
+                else if (imc >= 25 && imc <= 29.9)
+                {
+                    resultado = "Tiene sobre peso";
+                }
+                else
+                {
+                    resultado = "Tienes obesidad, cuidarte";
+                }
+                DisplayAlert("Resultado: ", resultado, "Quitar");
+            }
+            else
+            {
+                DisplayAlert("Datos erroneos", "Por favol, llena todo la informacion", "Quitar");
+            }
 
         }
     }
